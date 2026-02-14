26cbb67876b717dc56ef5c2b03435c9bdb4a9ff3
Jaco van Stryp <jacovanstryp@gmail.com>
2026-02-15 11:41:16 +1300

    generate files

---
+++ InvoicerClient/src/app/api/api/api.ts
@@ -0,0 +1,13 @@
+export * from './auth.service';
+import { AuthService } from './auth.service';
+export * from './client.service';
+import { ClientService } from './client.service';
+export * from './company.service';
+import { CompanyService } from './company.service';
+export * from './file.service';
+import { FileService } from './file.service';
+export * from './invoice.service';
+import { InvoiceService } from './invoice.service';
+export * from './product.service';
+import { ProductService } from './product.service';
+export const APIS = [AuthService, ClientService, CompanyService, FileService, InvoiceService, ProductService];
--- InvoicerClient/src/app/app.routes.ts
+++ InvoicerClient/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './Components/Auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+    title: 'Login',
+  },
+];
